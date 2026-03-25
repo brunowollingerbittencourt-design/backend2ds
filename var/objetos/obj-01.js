c8acd2c5a2bb6a5a46e94dd86fdfef7ba5d2a8d0
brunowollingerbittencourt-design <bruno.wollinger.bittencourt@escola.pr.gov.br>
2026-03-25 11:37:16 +0000

    finalizacao de objetos js

---
+++ var/objetos/obj-01.js
@@ -0,0 +1,15 @@
+// objetos são estruturas chaves-valor
+// Tambem chamadas "key-value"
+// objetos podem conter: arrays, string
+// number, objetcs, function..
+// possuem metodos proprios
+
+const jogador = {
+    nome: " Bruno ",
+    gols: 390,
+    time: ['Palmeiras', 'coxa', 'flamengo', 'Corinthians']
+}
+
+console.log (jogador.nome)
+console.log (jogador.gols)
+console.log (jogador.time)
---
+++ var/objetos/obj-02.js
@@ -0,0 +1,17 @@
+// a partir do exemplo de objeto crie: 
+// um objeto com o seu tema
+// deve possuir no minimo 6 propriedades
+// imprima de um jeito amigavel
+// exemplo: console.log("Cor do veiculo: " + carro.cor)
+
+
+const jaguar = {
+nome: jaguar,
+cor: [preta],
+valor: 300.000
+
+}
+
+console.log("nome do carro: " + jaguar.nome)
+console.log("cor do carro: " + jaguar.cor)
+console.log("valor do carro: " + jaguar.valor)
