b6e0247e372487a8c10d26db7e4dadaae3a220df
brunowollingerbittencourt-design <bruno.wollinger.bittencourt@escola.pr.gov.br>
2026-08-17 13:40:41 +0000

    implementa servidor HTTP com rotas para páginas inicial, sobre e erro 404

---
+++ 2ºTRIMESTRE/modulos/mod02-ESM/main-gato-mod.js
@@ -0,0 +1,8 @@
+// apenas importa modulos de
+// outros arquivos ( import )
+
+import {banhoGato, tosaGato, alimentarGato} from './gato-mod.js'
+
+console.log(banhoGato('Garfield', 'Completo'))
+console.log(tosaGato('Siamês', 'Bem baixinha'))
+console.log(alimentarGato('Persa', 'Petisco'))
--- 2ºTRIMESTRE/modulos/mod03-server/server.js
+++ 2ºTRIMESTRE/modulos/mod03-server/server.js
@@ -1,13 +1,35 @@
 const http = require('node:http')
-const porta = 8005
+const path = require('node:path')
+const fs = require('node:fs')
+const porta = 7777
+// Carrega o caminho dos arquivos
+const home = path.join(__dirname, 'Pages/index.html')
+const sobre = path.join(__dirname, 'Pages/sobre.html')
+const error = path.join(__dirname, 'Pages/error.html')
 
-const server = http.createServer((req, res)=>{
-     //qual informacao tenho?
-     res.setHeader('Content-Type', 'text/plain')
-     res.end('Bem vindo(a)! \nHomepage \\o/')
+const server = http.createServer((req, res) => {
+   // qual informação eu tenho?
+   const urlTratada = new URL(req.url, `http://${req.headers.host}`) //limpa a url e transforma em objeto
+   const recurso =  urlTratada.pathname //em qual rotas estamos?
+
+   if(recurso === '/') { //Página inicial
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    return res.end(fs.readFileSync(home, 'utf-8'));
+   }
+   if(recurso === '/sobre') {
+    res.statusCode = 200; //Sobre nós
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    return res.end(fs.readFileSync(sobre, 'utf-8'));
+   }
+   else {  
+    res.statusCode = 404; //404
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    return res.end(fs.readFileSync(error, 'utf-8'));
+   }
+
+});
+
+server.listen(porta, () => {
+  console.log(`Servidor rodando na porta ${porta}`)
 })
-server.listen(porta, ()=>{
-console.log(`Server rodando na porta $`)
-})
-
-
